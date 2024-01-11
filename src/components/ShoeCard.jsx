ee2c009a41697a21e4e886496b16a9050a68597f
ante-neh <amareanteneh12@gmail.com>
2024-01-11 10:27:41 +0300

    add the functionality and style

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -1,6 +1,28 @@
-const ShoeCard = () => {
+const ShoeCard = ({ imgUrl, changeBigShoeImg, bigShoeImg }) => {
+  const handleClick = ()=>{
+    if(bigShoeImg !== imgUrl.bigShoeImg){
+      changeBigShoeImg(imgUrl.bigShoe)
+    }
+  }
+
+
   return (
-    <div>ShoeCard</div>
+    <div className={`border-2 rounded-xl cursor-pointer max-sm:flex-1
+                    ${bigShoeImg === imgUrl.bigShoe? 'border-coral-red':'border-transparent'}
+                  `}
+                    onClick = {handleClick}
+                  >
+                <div className="flex justify-center items-center rounded-xl bg-card bg-center bg-cover sm:w-40 sm:h-40 max-sm:p-4">
+                  <img
+                  src = {imgUrl.thumbnail}
+                  alt = "shoes"
+                  width={127}
+                  height={103}
+                  className="object-contain"
+                  />
+
+                </div>
+    </div>
   )
 }
 
